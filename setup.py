6341566eabbd170dc492a73e6c6eee6342ea9c7d
vincent d warmerdam <vincentwarmerdam@gmail.com>
2024-12-17 17:54:47 +0100

    version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
 
 setup(
     name="wigglystuff",
-    version="0.1.6",
+    version="0.1.7",
     description="Collection of Jupyter Widgets",
     author="Vincent D. Warmerdam",
     packages=find_packages(exclude=["notebooks"]),
