46b59bfe5a7a7bf539b9653d42ac37f22e38d589
koaning <vincentwarmerdam@gmail.com>
2025-12-12 19:56:14 +0100

    demo

---
+++ demos/drivertour.py
@@ -0,0 +1,67 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "anywidget==0.9.21",
+#     "numpy==2.3.5",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.18.4"
+app = marimo.App(width="columns", sql_output="polars")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    from wigglystuff import DriverTour
+    return DriverTour, mo
+
+
+@app.cell
+def _(DriverTour, mo):
+    tour = mo.ui.anywidget(
+        DriverTour(steps=[
+            {
+                "element": ".marimo-cell",
+                "index": 0,
+                "popover": {
+                    "title": "Welcome!",
+                    "description": "In this first cell we do a bunch of imports.",
+                    "position": "center"
+                }
+            },
+            { 
+                "element": ".marimo-cell",
+                "index": 1,
+                "popover": {
+                    "title": "Fancy!",
+                    "description": "But in this cell we define a tour! Fancy that!",
+                    "position": "center"
+                }
+            }, 
+            { 
+                "element": ".marimo-cell",
+                "index": 2,
+                "popover": {
+                    "title": "Steps",
+                    "description": "You can also inspect the steps at the end here.",
+                    "position": "center"
+                }
+            }, 
+        
+        ])
+    )
+    tour
+    return (tour,)
+
+
+@app.cell
+def _(tour):
+    tour.steps
+    return
+
+
+if __name__ == "__main__":
+    app.run()
