dde7fd293c5e6a8ecee962615e664665c1d12a8e
Vincent D. Warmerdam <vincentwarmerdam@gmail.com>
2026-01-01 22:01:06 +0100

    so much lighter

--- wigglystuff/static/copybutton.js
+++ wigglystuff/static/copybutton.js
@@ -2,27 +2,19 @@
 const SVG_NS = "http://www.w3.org/2000/svg";
 function createCopyIcon() {
   const svg = document.createElementNS(SVG_NS, "svg");
-  svg.setAttribute("viewBox", "0 0 24 24");
+  svg.setAttribute("viewBox", "0 0 15 15");
   svg.setAttribute("class", "copy-button-icon");
   svg.setAttribute("aria-hidden", "true");
   svg.setAttribute("focusable", "false");
-  const rectBack = document.createElementNS(SVG_NS, "rect");
-  rectBack.setAttribute("x", "7");
-  rectBack.setAttribute("y", "7");
-  rectBack.setAttribute("width", "10");
-  rectBack.setAttribute("height", "10");
-  rectBack.setAttribute("rx", "2");
-  rectBack.setAttribute("fill", "currentColor");
-  rectBack.setAttribute("fill-opacity", "0.35");
-  const rectFront = document.createElementNS(SVG_NS, "rect");
-  rectFront.setAttribute("x", "4");
-  rectFront.setAttribute("y", "4");
-  rectFront.setAttribute("width", "10");
-  rectFront.setAttribute("height", "10");
-  rectFront.setAttribute("rx", "2");
-  rectFront.setAttribute("fill", "currentColor");
-  svg.appendChild(rectBack);
-  svg.appendChild(rectFront);
+  const path = document.createElementNS(SVG_NS, "path");
+  path.setAttribute(
+    "d",
+    "M1 9.50006C1 10.3285 1.67157 11.0001 2.5 11.0001H4L4 10.0001H2.5C2.22386 10.0001 2 9.7762 2 9.50006L2 2.50006C2 2.22392 2.22386 2.00006 2.5 2.00006L9.5 2.00006C9.77614 2.00006 10 2.22392 10 2.50006V4.00002H5.5C4.67158 4.00002 4 4.67159 4 5.50002V12.5C4 13.3284 4.67158 14 5.5 14H12.5C13.3284 14 14 13.3284 14 12.5V5.50002C14 4.67159 13.3284 4.00002 12.5 4.00002H11V2.50006C11 1.67163 10.3284 1.00006 9.5 1.00006H2.5C1.67157 1.00006 1 1.67163 1 2.50006V9.50006ZM5 5.50002C5 5.22388 5.22386 5.00002 5.5 5.00002H12.5C12.7761 5.00002 13 5.22388 13 5.50002V12.5C13 12.7762 12.7761 13 12.5 13H5.5C5.22386 13 5 12.7762 5 12.5V5.50002Z"
+  );
+  path.setAttribute("fill", "currentColor");
+  path.setAttribute("fill-rule", "evenodd");
+  path.setAttribute("clip-rule", "evenodd");
+  svg.appendChild(path);
   return svg;
 }
 function fallbackCopy(text) {
